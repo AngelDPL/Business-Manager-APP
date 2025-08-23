1f740e9cf13dee50ee1ce12f276629e8643aa3c4
Angel Ponte <angeld0606@email.com>
2025-08-23 12:52:48 +0200

    components Navbar, Footer, Layout created and App.jsx updated whit routes

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -1,12 +1,24 @@
-import { HomePage } from "../pages/HomePage/HomePage";
-import { StoreProvider } from "../store/StoreContext";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { StoreProvider } from '../store/storeContext';
+
+
+import HomePage from "../pages/HomePage/HomePage"
+
+
 
 export const App = () => {
   return (
     <StoreProvider>
-      <div className="App">
-        <HomePage />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<HomePage />} />
+            </Route>
+            <Route path="*" element={<div>Page not found</div>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </StoreProvider>
   );
 };
---
+++ frontend/src/components/Layout/Layout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import { Navbar } from "../Navbar/Navbar";
+import { Footer } from "../Footer/Footer";
+
+export const Layout = () => {
+  return (
+    <div className="layout">
+      <Navbar />
+      <main className="main-content">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+ 
